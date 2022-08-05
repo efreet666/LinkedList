492dabf7591ca234488a50562d7e7cff2fefbf3b
Vlad <vladislav.bok@yandex.ru>
2022-08-05 19:08:41 +0300

    Initial Commit

---
+++ LinkedList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LinkedList
+//
+//  Created by Влад Бокин on 05.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
